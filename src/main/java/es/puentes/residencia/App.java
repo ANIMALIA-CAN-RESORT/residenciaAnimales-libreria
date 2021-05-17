0c42df9deb11b4313571909beefeda7ef5d7cd2d
Jose <joseluispuentes@hotmail.com>
2021-05-17 17:53:46 +0200

    prueba enlace repo

--- src/main/java/es/puentes/residencia/App.java
+++ src/main/java/es/puentes/residencia/App.java
@@ -11,6 +11,7 @@ public class App {
 	
 	public static void main(String[] args) {
 
+		System.out.println("prueba");
 ////		ObjectMapper mapper = new JsonMapper();
 ////		
 ////		try (BufferedReader buffer = new BufferedReader(
